4dcc58c878db0638ee5dca54140a31cd3d70ec4b
hqf0330 <hqf0330@gmail.com>
2024-12-15 17:00:04 +0800

    增加日期工具类 新老访客标记修复完成

--- realtime-dwd/realtime-dwd-base-log/src/main/java/org/mason77/app/DwdBaseLogApp.java
+++ realtime-dwd/realtime-dwd-base-log/src/main/java/org/mason77/app/DwdBaseLogApp.java
@@ -2,6 +2,11 @@ package org.mason77.app;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SideOutputDataStream;
@@ -12,12 +17,14 @@ import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
 import org.mason77.base.BaseApp;
 import org.mason77.constant.StreamConstant;
+import org.mason77.util.DateFormatUtil;
 import org.mason77.util.FlinkSinkUtil;
 
 public class DwdBaseLogApp extends BaseApp {
 
     // 注意范型擦除，匿名内部类
-    private static final OutputTag<String> dirtyTag = new OutputTag<String>("dirtyTag"){};
+    private static final OutputTag<String> dirtyTag = new OutputTag<String>("dirtyTag") {
+    };
 
     public static void main(String[] args) {
         new DwdBaseLogApp().start(10002, 3, "dwd_base_log", StreamConstant.KAFKA_LOG);
@@ -38,13 +45,56 @@ public class DwdBaseLogApp extends BaseApp {
             }
         });
 
-        jsonObjDS.print("main");
+        // jsonObjDS.print("main");
         SideOutputDataStream<String> dirtyDataDS = jsonObjDS.getSideOutput(dirtyTag);
         dirtyDataDS.print("dirty");
 
         // 脏数据写入到kafka中
         KafkaSink<String> dirtyDataSink = FlinkSinkUtil.getKafkaSink("dirty_data");
         dirtyDataDS.sinkTo(dirtyDataSink);
+
+        // 新老访客的标记修复
+        SingleOutputStreamOperator<JSONObject> fixedDS = jsonObjDS.keyBy(json -> json.getJSONObject("common").getString("mid"))
+                .map(new RichMapFunction<JSONObject, JSONObject>() {
+
+                    private ValueState<String> lastVisitDateState;
+
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        lastVisitDateState = getRuntimeContext().getState(new ValueStateDescriptor<>("lastVisitDate", String.class));
+                    }
+
+                    @Override
+                    public JSONObject map(JSONObject jsonObject) throws Exception {
+                        // 获取新否新用户
+                        String isNew = jsonObject.getJSONObject("common").getString("is_new");
+
+                        // 获取时间状态
+                        String lastVisitDate = lastVisitDateState.value();
+                        Long ts = jsonObject.getLong("ts");
+                        String curVisitDate = DateFormatUtil.tsToDate(ts);
+                        if("1".equals(isNew)) {
+
+                            if (StringUtils.isEmpty(lastVisitDate)) {
+                                lastVisitDateState.update(curVisitDate);
+                            } else {
+                                if (!lastVisitDate.equals(curVisitDate)) {
+                                    isNew = "0";
+                                    jsonObject.getJSONObject("common").put("is_new", isNew);
+                                }
+                            }
+
+                        } else {
+                            if (StringUtils.isEmpty(lastVisitDate)) {
+                                String yesterday = DateFormatUtil.tsToDate(ts - 24 * 60 * 60 * 1000);
+                                lastVisitDateState.update(yesterday);
+                            }
+                        }
+                        return jsonObject;
+                    }
+                });
+
+        fixedDS.print("fixed");
 
     }
 }
